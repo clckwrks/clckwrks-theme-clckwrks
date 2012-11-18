a8f2aaface4989312a42fcb6287b8c848a002306
Jeremy Shaw <jeremy@seereason.com>
2012-11-18 23:08:30 +0000

    clckwrks-plugin-bugs now (mostly?) working

--- Theme.hs
+++ Theme.hs
@@ -10,20 +10,28 @@ import HSP
 
 theme :: Theme
 theme = Theme
-    { themeName      = "bootstrap-theme"
+    { themeName      = "clckwrks"
     , _themeTemplate = pageTemplate
     }
 
-pageTemplate :: XMLGenT (ClckT ClckURL (ServerPartT IO)) XML
-pageTemplate =
+pageTemplate :: ( EmbedAsChild (ClckT ClckURL (ServerPartT IO)) headers
+                , EmbedAsChild (ClckT ClckURL (ServerPartT IO)) body
+                ) =>
+                Text
+             -> headers
+             -> body
+             -> XMLGenT (ClckT ClckURL (ServerPartT IO)) XML
+
+pageTemplate ttl hdr bdy =
     <html>
      <head>
-      <title><% getPageTitle %></title>
+      <title><% ttl %></title>
       <link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.1.1/css/bootstrap.min.css"        rel="stylesheet" media="screen" />
       <link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.1.1/css/bootstrap-responsive.css" rel="stylesheet" />
       <link rel="stylesheet" type="text/css" href=(ThemeData "style.css") />
       <link rel="stylesheet" type="text/css" href=(ThemeData "hscolour.css") />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <% hdr %>
       <% googleAnalytics %>
      </head>
      <body>
@@ -43,8 +51,9 @@ pageTemplate =
 
       <div class="container">
        <div class="row">
-        <h1><% getPageTitle %></h1>
-        <% getPageContent %>
+--        <h1><% getPageTitle %></h1>
+        <% bdy %>
+  --      <% getPageContent %>
        </div>
       </div>
       <script src="http://code.jquery.com/jquery-latest.js"></script>
